2e3c7a55d1f92ff7dd0dbbf40d1dd9de1c296d75
mdmominur <mominur__rahman@outlook.com>
2021-12-10 10:29:39 +0600

    take admin response and update in the app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Team Project</h1>
+      <h1>Hello team member</h1>
       <h1>Hello team membera</h1>
       <h4>Ui integration</h4>
     </div>
