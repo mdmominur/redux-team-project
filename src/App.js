b667ba1028fb5f5468da0eb1ecf016ace3a260ec
mdmominur <mominur__rahman@outlook.com>
2021-12-10 23:37:47 +0600

    Remove unnessary

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Team Project</h1>
-      <h1>Hello team member</h1>
-      <h1>Hello team membera</h1>
-      <h4>Ui integration</h4>
+      
     </div>
   );
 }
